8d56fcb52c4b1ad380055e461da4ff2096bca5dc
Frank Wunderlich <frank-w@public-files.de>
2020-12-05 16:13:01 +0100

    mt76: add different filesize-version without set_fs

--- drivers/net/wireless/mediatek/mt76/eeprom.c
+++ drivers/net/wireless/mediatek/mt76/eeprom.c
@@ -37,25 +37,38 @@ mt76_get_of_file(struct mt76_dev *dev, int len)
 	loff_t pos=0;
 	int ret;
 	int fsize;
+	struct inode *inode = NULL;
+	loff_t size;
 
 	ret = snprintf(path,sizeof(path),"/lib/firmware/mediatek/%s_rf.bin",dev->dev->driver->name);
 	if(ret<0)
 		return -EINVAL;
-	dev_info(dev->dev,"Load firmware : %s\n",path);
+	dev_info(dev->dev,"Load eeprom: %s\n",path);
 	fp = filp_open(path, O_RDONLY, 0);
 	if (IS_ERR(fp)) {
-		dev_info(dev->dev,"Open file failed : %s\n",path);
+		dev_info(dev->dev,"Open eeprom file failed: %s\n",path);
 		return -ENOENT;
 	}
-	fsize=mt76_get_file_size(path);
-	printk(KERN_ALERT "DEBUG: Passed %s %d fsize:%d \n",__FUNCTION__,__LINE__,fsize);
+	//fsize=mt76_get_file_size(path);
+	inode = file_inode(fp);
+	if ((!S_ISREG(inode->i_mode) && !S_ISBLK(inode->i_mode))) {
+		printk(KERN_ALERT "invalid file type: %s\n", path);
+		return -ENOENT;
+	}
+	size = i_size_read(inode->i_mapping->host);
+	printk(KERN_ALERT "DEBUG: Passed %s %d fsize:%lld \n",__FUNCTION__,__LINE__,size);
+	if (size < 0)
+	{
+		printk(KERN_ALERT "failed getting size of %s size:%lld \n",path,size);
+		return -ENOENT;
+	}
 	ret = kernel_read(fp, dev->eeprom.data, len, &pos);
-	if(ret < fsize){
-		dev_info(dev->dev,"Load firmware ERR, count %d byte (len:%d)\n",ret,len);
+	if(ret < size){
+		dev_info(dev->dev,"Load eeprom ERR, count %d byte (len:%d)\n",ret,len);
 		return -ENOENT;
 	}
 	filp_close(fp, 0);
-	dev_info(dev->dev,"Load firmware OK, count %d byte\n",ret);
+	dev_info(dev->dev,"Load eeprom OK, count %d byte\n",ret);
 
 	return 0;
 }
